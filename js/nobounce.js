d667b6340a273278f5acf4ed55492c134c62b4a6
Fred <fredk@alumni.usc.edu>
2018-12-19 15:06:02 -0800

    no bounce

--- js/nobounce.js
+++ js/nobounce.js
@@ -1,8 +1,17 @@
-// Disable overscroll / viewport moving on everything but scrollable divs
-$('body').on('scroll', function (e) {
-        if (!$('.scrollable').has($(e.target)).length) e.preventDefault();
+//uses document because document will be topmost level in bubbling
+$(document).on('touchmove',function(e){
+  e.preventDefault();
 });
-
-$('body').on('touchmove', function (e) {
-        if (!$('.scrollable').has($(e.target)).length) e.preventDefault();
+//uses body because jquery on events are called off of the element they are
+//added to, so bubbling would not work if we used document instead.
+$('body').on('touchstart','.scrollable',function(e) {
+  if (e.currentTarget.scrollTop === 0) {
+    e.currentTarget.scrollTop = 1;
+  } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+    e.currentTarget.scrollTop -= 1;
+  }
+});
+//prevents preventDefault from being called on document if it sees a scrollable div
+$('body').on('touchmove','.scrollable',function(e) {
+  e.stopPropagation();
 });
